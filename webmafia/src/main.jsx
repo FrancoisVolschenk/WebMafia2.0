e54cd39e7d201c861ef8cb2f978991c2afd42690
FrancoisVolschenk <francoisv934@gmail.com>
2025-07-07 23:46:46 +0200

    Added basic laout of landing page

--- webmafia/src/main.jsx
+++ webmafia/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Landing from './views/landing.jsx'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -10,6 +10,6 @@ import '@fontsource/roboto/700.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Landing />
   </StrictMode>,
 )
